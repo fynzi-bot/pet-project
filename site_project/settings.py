407dbb787ce69aaa3ccf1d85705687482c5f1bc4
qcold <bogomolov.1523@mail.ru>
2026-05-15 11:14:14 +0300

    deploy this fuck

--- site_project/settings.py
+++ site_project/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['static']
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -127,3 +127,4 @@ LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/admin/login'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+ALLOWED_HOSTS = ['*']
