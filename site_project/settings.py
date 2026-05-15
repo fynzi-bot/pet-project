50600522789e95528736472ab210581723e23499
qcold <bogomolov.1523@mail.ru>
2026-05-15 14:54:28 +0300

    vrode dolzhno rabotat

--- site_project/settings.py
+++ site_project/settings.py
@@ -16,7 +16,7 @@ from django.conf.global_settings import MEDIA_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-ALLOWED_HOSTS = ['.vercel.app', 'pet-project-pi.vercel.app']
+ALLOWED_HOSTS = ['*']
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
